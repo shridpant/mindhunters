dadd4afc39354cca7aed8523162e4111f10c1a88
shridpant <shrid.pant8@gmail.com>
2020-12-27 11:17:58 +0545

    Updated error handling, introduced like

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2020 Shrid Pant
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os
 from functools import wraps
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -14,6 +36,7 @@ from src.helpers import UserInfo, login_required, error
 
 # Configure application
 app = Flask(__name__)
+# Might encounter browser caching problem. Force refresh using CTRL+F5
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure session to use filesystem (instead of signed cookies)
@@ -126,21 +149,24 @@ def search():
 @app.route("/<string:target>")
 @login_required
 def LookupProfiles(target):
-    match = db.execute("SELECT * FROM users WHERE username=:target", target=target)
-    if not match:
-        return error("Page not found!", 404)
-    # DP Search
-    dp = "static/dp/" + match[0]['username'] + "." + match[0]['dp']
-    if not os.path.exists(dp):
-        dp = "../static/dp/"+"default.png"
-    else:
-        dp = "../static/dp/" + match[0]['username'] + "." + match[0]['dp']
-    # Profile Feed
-    get_posts = db.execute("SELECT * FROM :tablename", tablename=match[0]['username'])
-    if get_posts:
-        return render_template("found_profile.html", dp=dp, user=match[0], posts=get_posts, reputation=((match[0]['score']/match[0]['total'])*10))
-    else:
-        return render_template("found_profile.html", dp=dp, user=match[0], reputation=((match[0]['score']/match[0]['total'])*10))
+    try:
+        match = db.execute("SELECT * FROM users WHERE username=:target", target=target)
+        if not match:
+            return error("Page not found!", 404)
+        # DP Search
+        dp = "static/dp/" + match[0]['username'] + "." + match[0]['dp']
+        if not os.path.exists(dp):
+            dp = "../static/dp/"+"default.png"
+        else:
+            dp = "../static/dp/" + match[0]['username'] + "." + match[0]['dp']
+        # Profile Feed
+        get_posts = db.execute("SELECT * FROM :tablename", tablename=match[0]['username'])
+        if get_posts:
+            return render_template("found_profile.html", dp=dp, user=match[0], posts=get_posts, reputation=((match[0]['score']/match[0]['total'])*10))
+        else:
+            return render_template("found_profile.html", dp=dp, user=match[0], reputation=((match[0]['score']/match[0]['total'])*10))
+    except Exception as msg:
+        return error(str(msg))
 
 
 @app.route("/about", methods=["GET"])
@@ -151,26 +177,38 @@ def about():
 @app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
-    userInfo, dp = UserInfo(db)
-    if request.method == "GET":
-        return render_template("profile.html", userInfo=userInfo, dp=dp, reputation=((userInfo['score']/userInfo['total'])*10))
-    else:
-        new_bio = request.form.get("bio")
-        if request.form.get("dp_submit"):
-            dp_file = request.files['dp_upload']
-        else :
-            dp_file=""
-        if dp_file:
-            filename = secure_filename(dp_file.filename)
-            file_extension = filename.rsplit('.', 1)[1].lower()
-            db.execute("UPDATE users SET dp=:new_dp WHERE id=:user_id", new_dp=file_extension, user_id=session["user_id"])
-            dp_file.save('static/dp/' + userInfo['username'] + "." + file_extension)
-            return redirect("/profile")
-        elif new_bio:
-            db.execute("UPDATE users SET bio=:new_bio WHERE id=:user_id", new_bio=new_bio, user_id=session["user_id"])
-            return redirect("/profile")
+    try:
+        userInfo, dp = UserInfo(db)
+        if request.method == "GET":
+            get_posts = db.execute("SELECT * FROM :tablename", tablename=userInfo['username'])
+            if get_posts:
+                return render_template("profile.html", userInfo=userInfo, dp=dp, posts=get_posts, reputation=((userInfo['score']/userInfo['total'])*10))
+            else:
+                return render_template("profile.html", userInfo=userInfo, dp=dp, reputation=((userInfo['score']/userInfo['total'])*10))
         else:
-            return redirect("/profile")
+            new_bio = request.form.get("bio")
+            like_value = request.json['data']
+            if request.form.get("dp_submit"):
+                dp_file = request.files['dp_upload']
+            else :
+                dp_file=""
+            if dp_file:
+                filename = secure_filename(dp_file.filename)
+                file_extension = filename.rsplit('.', 1)[1].lower()
+                db.execute("UPDATE users SET dp=:new_dp WHERE id=:user_id", new_dp=file_extension, user_id=session["user_id"])
+                dp_file.save('static/dp/' + userInfo['username'] + "." + file_extension)
+                return redirect("/profile")
+            elif new_bio:
+                db.execute("UPDATE users SET bio=:new_bio WHERE id=:user_id", new_bio=new_bio, user_id=session["user_id"])
+                return redirect("/profile")
+            elif like_value:
+                if like_value:
+                    db.execute("UPDATE :tablename SET likes=likes+1 WHERE id=:like_id", like_id=like_value)
+                    return jsonify(status="success")
+            else:
+                return redirect("/profile")
+    except Exception as msg:
+        return error(str(msg))
 
 # Error handler
 def errorhandler(e):
